d9143f2cce7a4deeb4c610d1538970cc8dc0d7fa
jaykumarM5 <jaikumar.saganjel@gmail.com>
2020-09-29 23:14:04 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { TodoHeaderComponent } from './todo/todo-header/todo-header.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoInputComponent } from './todo/todo-input/todo-input.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoHeaderComponent,
+    TodoFooterComponent,
+    TodoInputComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
